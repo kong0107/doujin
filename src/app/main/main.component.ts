efe4626d43ac0cc593b8220a5873254daea4bd15
kong0107 <kong0107@gmail.com>
2018-03-04 00:35:36 +0800

    start show/hide system

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,29 +9,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class MainComponent implements OnInit {
     articleGroups = [];
-    contract = {
-        category: "",
-        workType: "",
-        delegation_name: "BBB"
-    };
-    showSetting = {
-        category: false
-    };
-    setting = {
-        category: {
-            display: true,
-            value: ""
-        }
-    };
-    
-    set(input) {
-        for(let key in input) {
-            this.setting[key].display = false;
-            this.setting[key].value = input[key];
-        }
-    }
+    settings: any= {};
 
     variables = {
+        契約類型: "category",
+        商家出資: "contributor",
+
+        委託資料及委託規格: "delegation_data",
+        工作方式: "workType",
+
         草稿繳交期限: "draftDeadline",
         定稿繳交期限: "finalDeadline",
         付全額期間: "fullPayPeriod",
@@ -42,8 +28,101 @@ export class MainComponent implements OnInit {
         改稿次數: "draftFixNumber",
         草稿小修回應期間: "draftFixRespondPeriod",
         驗收期間: "acceptPeriod",
-        委託名稱: "delegation_name"
+        付定金期間:"earnestMoneyPeriod",
+        定金數額: "earnestMoneyAmount",
+        付尾款期間: "finalPayPeriod",
+        尾款數額: "finalPayAmount",
+
+        繪師姓名: "a_name",
+        繪師戶籍地址: "a_permanentAddress",
+        繪師身分證號: "a_id",
+        繪師電子郵件: "a_email",
+        繪師通訊地址: "a_correspondenceAddress",
+        繪師連絡電話: "a_phone",
+
+        商家名稱: "b_name",
+        商家登記地址: "b_permanentAddress",
+        商家統一編號: "b_id",
+        商家代表人: "b_chairperson",
+        商家通訊地址: "b_correspondenceAddress",
+        商家聯絡電話: "b_phone",
+
+        委託主姓名: "b_name",
+        委託主戶籍地址: "b_permanantAddress",
+        委託主身分證號: "b_id",
+        委託主電子郵件: "b_email",
+        委託主通訊地址: "b_correspondenceAddress",
+        委託主連絡電話: "b_phone",
+
+        繪師帳戶銀行名稱: "bank",
+        繪師戶名: "accountName",
+        繪師帳號: "accountNumber",
+
+        委託名稱: "delegation_name",
+        委託內容: "delegation_content",
+        委託規格: "delegation_format",
+        重大修改: "seriousityDefinition",
+
+        著作權條款: "copyrightTerms",
+        著作財產權歸屬: "copyrightBelonging",
+        繪師指定公開發表名稱: "a_pseudonym",
+        繪師利用範圍: "a_useRange"
     };
+
+    set(attribute, value) {
+        let s = this.settings;
+        s[attribute].value = value;
+        s[attribute].decided = true;
+        switch(attribute) {
+            case "category":
+                s[attribute].display = false;
+                switch(value) {
+                    case "設定圖":
+                    case "非出版單張":
+                        s.delegation_data.display = true;
+                        s.workType.display = true;
+                        break;
+                    case "出版插圖":
+                    case "合本":
+                        s.contributor.display = true;
+                        break;
+                    default:
+                        throw new Error;
+                }
+                break;
+            case "contributor":
+                s[attribute].display = false;
+                switch(s.category.value) {
+                    case "出版插圖":
+                        s.delegation_data.display = true;
+                        s.workType.display = true;
+                        break;
+                    case "合本":
+                        console.log("合本契約");
+                        break;
+                    default:
+                        throw new Error;
+                }
+                break;
+            case "workType":
+            case "copyrightBelonging":
+                break;
+            default:
+                throw new Error;
+        }
+    }
+    unset(attribute) {
+        this.settings[attribute].display = true;
+        this.settings[attribute].decided = false;
+    }
+
+    // deprecated
+    contract = {
+        category: "",
+        workType: "",
+        delegation_name: "BBB"
+    };
+
 
   chineseNumber(input: number) {
     const digit = "零一二三四五六七八九";
@@ -59,7 +138,13 @@ export class MainComponent implements OnInit {
     return result;
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    for(let key in this.variables) {
+      let attribute = this.variables[key];
+      this.settings[attribute] = {display: false, decided: false, value: ""};
+    }
+    this.settings.category.display = true;
+  }
 
   ngOnInit() {
       this.http.get<ArticleGroup[]>("/assets/articles.json")
